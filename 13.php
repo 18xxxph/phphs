778ec9f074208f6b80a7ded6484cb31f13665d8a
18xxxph <49505197+18xxxph@users.noreply.github.com>
2019-06-06 08:47:08 +0800

    Add files via upload

---
+++ 13.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>php函数</title>
+</head>
+
+<?php
+function writeName($fname)
+{
+    echo $fname . " Refsnes.<br>";
+}
+ 
+echo "My name is ";
+writeName("Kai Jim");
+echo "My sister's name is ";
+writeName("Hege");
+echo "My brother's name is ";
+writeName("Stale");
+?>
+
+
+
+
+
+
+<body>
+</body>
+</html>
