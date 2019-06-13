1b59908096c9c11738a375070c8ef6b0d7a4cfba
18xxxph <49505197+18xxxph@users.noreply.github.com>
2019-06-13 08:37:01 +0800

    Add files via upload

---
+++ 14.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>魔术常量</title>
+</head>
+<?php
+class test {
+    function _print() {
+        echo '类名为：'  . __CLASS__ . "<br>";
+        echo  '函数名为：' . __FUNCTION__ ;
+    }
+}
+$t = new test();
+$t->_print();
+?>
+
+
+
+<body>
+</body>
+</html>
